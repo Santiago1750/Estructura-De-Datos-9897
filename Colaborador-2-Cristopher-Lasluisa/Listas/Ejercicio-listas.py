7738ff0fe4e8edf5313d3bd5f5bb2b23565e5ca4
CristopherLasluisa:) <132636689+Cristopher-10@users.noreply.github.com>
2023-05-20 21:38:26 -0500

    He creado un ejemplo de listas utilizando como ejemplo algunas frutas

--- Colaborador-2-Cristopher-Lasluisa/Listas/Ejercicio-listas.py
+++ Colaborador-2-Cristopher-Lasluisa/Listas/Ejercicio-listas.py
@@ -1,1 +1,35 @@
+# Crear una lista vacía
+mi_lista = []
+
+# Agregar elementos a la lista
+mi_lista.append("Manzana")
+mi_lista.append("Banana")
+mi_lista.append("Cereza")
+
+# Acceder a un elemento de la lista por su índice
+print(mi_lista[0])  # Imprime "Manzana"
+
+# Modificar un elemento de la lista
+mi_lista[1] = "Naranja"
+
+# Recorrer la lista e imprimir cada elemento
+for fruta in mi_lista:
+    print(fruta)
+
+# Obtener la longitud de la lista
+longitud = len(mi_lista)
+print("La longitud de la lista es:", longitud)
+
+# Verificar si un elemento está en la lista
+if "Cereza" in mi_lista:
+    print("La cereza está en la lista")
+
+# Eliminar un elemento de la lista
+mi_lista.remove("Manzana")
+
+# Ordenar la lista alfabéticamente
+mi_lista.sort()
+
+# Imprimir la lista completa
+print(mi_lista)
 
